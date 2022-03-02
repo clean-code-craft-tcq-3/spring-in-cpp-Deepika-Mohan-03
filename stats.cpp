fd10ebbdc14891e5c81ecddb85bb9333c34fe96f
Deepika-Mohan-03 <99876034+Deepika-Mohan-03@users.noreply.github.com>
2022-03-02 16:24:12 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,23 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& R) {
     //Implement statistics here
+    /*
+    float minimum = *min_element(R.begin() , R.end())
+    float min = R[0] , max = R[0], avg;
+    for(float x : R)
+    {
+        if(x < min)
+        {
+           min = x;
+        }
+    }
+    for(float x : R)
+    {
+        if(x > max)
+        {
+           max = x;
+        }
+    }
+    */
 }
