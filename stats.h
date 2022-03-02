e4162ae117df8d0c4513deafd684d26d11828682
Deepika-Mohan-03 <99876034+Deepika-Mohan-03@users.noreply.github.com>
2022-03-02 17:51:27 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,6 +6,49 @@ class Stats
    public:
     float min, max, average;
 };
+class IAlerter{
+public:
+    virtual void alertsystem() = 0;
+};
+
+class EmailAlert:public IAlerter
+{
+   public:
+    bool emailSent = 0;
+    void alertsystem()
+    {
+        //Send email
+        emailSent = 1;
+    }
+};
+
+class LEDAlert:public IAlerter
+{
+    public:
+     bool ledGlows = 0;
+     void alertsystem()
+    {
+        //Turn On LED
+        ledGlows = 1;
+    }
+};
+
+class StatsAlerter:public IAlerter
+{
+   public:
+    float Max_value;
+    std::vector<IAlerter*> Alert;
+    StatsAlerter(const float &M,std::vector<IAlerter*> &A)
+    {
+    Max_value = M;
+    Alert = A;
+    }
+    void alertsystem()
+    {
+    }
+    void checkAndAlert(const std::vector<float>& );
+};
+
 
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& R);
