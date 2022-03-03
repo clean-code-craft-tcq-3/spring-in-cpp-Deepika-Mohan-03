25152a08e2cdf83e861d16733f34c2a6c6818f8a
Deepika-Mohan-03 <99876034+Deepika-Mohan-03@users.noreply.github.com>
2022-03-03 19:27:01 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -48,10 +48,13 @@ void StatsAlerter::checkAndAlert(const std::vector<float>&Readings)
     }
     if(max > thersholdvalue)
     {
+	    EmailAlert::ealert();
+	    LEDAlert::Lalert();
+	 /*   
         for(int y = 0; y < Alert.size(); y++)
         {
            Alert[y]->alert();
-        }
+        } */
     }
 }
     
